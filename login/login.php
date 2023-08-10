0f94a986c18476e1ed8b4eee8a92c04ab19d3905
pallaasda <gantengnopal123@gmail.com>
2023-08-10 19:59:27 +0700

    login php

--- login/login.php
+++ login/login.php
@@ -1,6 +1,3 @@
-<!-- <?php
-    // include 'library/koneksi.php';
-    ?> -->
     <!DOCTYPE html>
     <html lang="en">
     
@@ -11,7 +8,6 @@
         <title>M10 - WMS</title>
         <link rel="stylesheet" href="../asset/css/style.css">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-        <!-- <link href='asset/css/bootstrap.css' rel='stylesheet'> -->
     </head>
     
     <body>
@@ -21,7 +17,7 @@
             <span class="bg-animate2"></span>
     
             <div class="form-box login">
-                <h2 class="animation" style="--i:0; --j:17;">Login</h2>
+                <h2 class="animation" style="--i:0; --j:17;">Login Admin</h2>
                 <form action="#" method="POST">
                     <div class="input-box animation" style="--i:1; --j:18;">
                         <input type="username" name="username" required>
@@ -38,26 +34,9 @@
                         <p>belum memiliki akun? <a href="#" class="register-link">Daftar</a></p>
                     </div>
                 </form>
-    
-                <?php
-                    // if (isset($_POST['btn'])) {
-                    //     $user = $_POST['username'];
-                    //     $pass = md5($_POST['password']);
-                    //     $result = mysqli_query($con, "SELECT*FROM tb_user WHERE username='$user' AND password ='$pass'");
-                    //     $row = mysqli_num_rows($result);
-                    //     $raql = mysqli_fetch_assoc($result);
-    
-                    //     if ($row == 1) {
-                    //     // $_SESSION['username']=$result['email'];
-                    //         Echo "ber<script>alert(' berhasil');window.location.href='admin/index.php'</script>hasil";
-                    //     }else{
-                    //         echo "<script>alert('tidak berhasil')</script>";
-                    //     }
-                    // }
-                ?>
             </div>
             <div class="info-text login">
-                <h2 class="animation" style="--i:0; --j:17;">Mitra10 Indonesia</h2>
+                <h2 class="animation" style="--i:0; --j:17;">Warehouse Management System</h2>
                 <p class="animation" style="--i:1; --j:18;">enjoy the experience of managing stock with a modern system</p>
             </div>
             
@@ -83,26 +62,11 @@
                     <div class="logreg-link animation" style="--i:22; --j:5;">
                         <p>sudah memiliki akun? <a href="#" class="login-link">Login</a></p>
                     </div>
-    
-                    <?php
-                        // if (isset($_POST['submit'])) {
-                        //     $user=$_POST['username'];
-                        //     $email=$_POST['email'];
-                        //     $pass=MD5($_POST['password']);
-        
-                        //     $result = mysqli_query($con, "INSERT INTO tb_user VALUES('','$user','$email','$pass')");
-                        //     if ($result) {
-                        //         echo "berhasil";
-                        //     }else{
-                        //         echo "tidak berhasil";
-                        //     }
-                        // }
-                    ?>
                 </form>
             </div>
             <div class="info-text register">
-                <h2 class="animation" style="--i:17; --j:0;">Mitra10 Indonesia</h2>
-                <p class="animation" style="--i:18; --j:1;">Warehouse Management System Native</p>
+                <h2 class="animation" style="--i:17; --j:0;"></h2>
+                <p class="animation" style="--i:18; --j:1;">Warehouse Management System </p>
             </div>
     
         </div>
