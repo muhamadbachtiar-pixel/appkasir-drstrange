d971433659aa900b1984fbfeb6d7c8b00873559d
pallaasda <gantengnopal123@gmail.com>
2023-08-16 16:51:24 +0700

    commit

--- admin/modul/delete-barang.php
+++ admin/modul/delete-barang.php
@@ -1,1 +1,11 @@
-asdasd
+<?php   
+    include './library/koneksi.php';
+    if(isset($_GET['delete'])){
+        $id = $_GET['delete'];
+
+        $sql = mysqli_query($con, "DELETE FROM tb_barang WHERE id_barang = '$id'");
+        if ($sql) {
+            header('location:http://localhost/appkasir-drstrange/admin/index.php?page=input-barang');
+        }
+    }
+?>
