c721bbc2993dde70be71b832f27c130ba087bc57
pallaasda <gantengnopal123@gmail.com>
2023-08-17 09:51:14 +0700

    commit

--- admin/modul/update-barang.php
+++ admin/modul/update-barang.php
@@ -18,13 +18,7 @@
 ?>
     <form action="komentar/edit.php" method="post">
     <div class="form-group">
-            <input name="id_komentar" value="<?php echo $id_komentar; ?>" type="hidden" class="form-control">
-            <input name="id_artikel" value="<?php echo $id_artikel; ?>" type="hidden" class="form-control">
         </div>
-        <!-- <div class="form-group">
-            <label>Artikel:</label>
-            <input name="nama" value="<?php echo $judul_artikel; ?>" type="text" class="form-control" disabled>
-        </div> -->
         <div class="form-group">
             <label>Nama Barang:</label>
             <input name="nama" value="<?php echo $nama_barang; ?>" type="text" class="form-control" placeholder="Masukan nama" required>
@@ -34,7 +28,7 @@
             <div class="col-sm-12">
                 <div class="form-group">
                     <label>Gambar Barang:</label>
-                    <input name="gambar" value="<?php echo $gambar_brng; ?>" type="file" class="form-control" placeholder="Masukan email" required>
+                    <input name="gambar"  type="file" class="form-control" value="<?php echo $gambar_brng; ?>" required>
                 </div>
             </div>
         </div>
@@ -52,42 +46,30 @@
             <div class="col-sm-12">
                 <div class="form-group">
                     <label>Jenis Barang:</label>
-                    <select name="jenis" value="<?php echo $jenis_barang; ?>" required>
+                    <select class="form-control" name="jenis" value="<?php echo $jenis_barang; ?>" required>
                     <?php
-
+                        $sql = mysqli_query($con, "SELECT * FROM tb_kategori");
+                        while ($row = mysqli_fetch_array($sql)) {
+                            echo "<option value='$row[jenis_brng]'>$row[jenis_brng]</option>";
+                        }
                     ?>
-                        <!-- <option value="elektronik">Elektronik</option>
-                        <option value="kecantikan">Kecantikan</option>
-                        <option value="peralatan">Peralatan</option>
-                        <option value="kendaraan">Kendaraan</option>
-                        <option value="kendaraan">Narkotika</option> -->
                     </select>
                 </div>
             </div>
         </div>
-   
         <div class="row">
             <div class="col-sm-12">
                 <div class="form-group">
-                    <label>Komentar:</label>
-                    <textarea name="isi_komentar" class="form-control" rows="5"><?php echo $isi_komentar; ?></textarea>
+                    <label>Deskripsi Barang:</label>
+                    <input name="harga" value="<?php echo $desk_barang; ?>" type="text" class="form-control" placeholder="Masukan email" required>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-sm-12">
                 <div class="form-group">
-                <label>Status:</label>
-                    <div class="form-check-inline">
-                        <label class="form-check-label">
-                        <input type="radio" <?php if ($data['status_komentar']==1) echo "checked"; ?> class="form-check-input" name="status" value="1">Publish
-                        </label>
-                    </div>
-                    <div class="form-check-inline">
-                        <label class="form-check-label">
-                        <input type="radio" <?php if ($data['status_komentar']==0) echo "checked"; ?> class="form-check-input" name="status" value="0">Tidak dipublish
-                        </label>
-                    </div>
+                    <label>Deskripsi Barang:</label>
+                    <input name="harga" value="<?php echo $jumlah_barang; ?>" type="text" class="form-control" placeholder="Masukan email" required>
                 </div>
             </div>
         </div>
@@ -107,25 +89,12 @@
             $data = htmlspecialchars($data);
             return $data;
         }
-        $id_komentar=input($_POST["id_komentar"]);
-        $nama=input($_POST["nama"]);
-        $email=input($_POST["email"]);
-        $isi_komentar=input($_POST["isi_komentar"]);
-        $id_artikel=input($_POST["id_artikel"]);
-        $status=input($_POST["status"]);
-
 
         //Query input menginput data kedalam tabel anggota
-        $sql="update komentar set
-        nama='$nama',
-        email='$email',
-        isi_komentar='$isi_komentar',
-        id_artikel='$id_artikel',
-        status_komentar='$status'
-        where id_komentar=$id_komentar";
-
+        
         //Mengeksekusi/menjalankan query 
-        $hasil=mysqli_query($con,$sql);
+        $hasil=mysqli_query($con, "UPDATE tb_barang SET 
+                                    ");
 
 
         //Kondisi apakah berhasil atau tidak dalam mengeksekusi query diatas
