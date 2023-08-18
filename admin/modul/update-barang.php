49226228f16599d7d2c878f967dfe7300ebf317c
pallaasda <gantengnopal123@gmail.com>
2023-08-18 22:46:49 +0700

    commit

--- admin/modul/update-barang.php
+++ admin/modul/update-barang.php
@@ -1,34 +1,22 @@
 
 <?php
-    $id_barang=$_POST["id_barang"];
-    // mengambil data barang dengan kode paling besar
     include '../library/koneksi.php';
-    $query = mysqli_query($con, "SELECT * FROM tb_barang where id_barang=$id_barang  ");
-    $data = mysqli_fetch_array($query); 
-
-    // $judul_artikel=$data['judul_artikel'];
-    $nama_barang=$data['nama_brng'];
-    $gambar_brng=$data['gambar_brng'];
-    $harga_barang=$data['harga_brng'];
-    $jenis_barang=$data['jenis_brng'];
-    $desk_barang=$data['deskripsi'];
-    $jumlah_barang=$data['jumlah_brng'];
-    // $id_artikel=$data['id_artikel'];
-
+    $sql = mysqli_query($con,"SELECT * FROM tb_barang");
+    $row = mysqli_fetch_array($sql);
 ?>
     <form action="komentar/edit.php" method="post">
     <div class="form-group">
         </div>
         <div class="form-group">
             <label>Nama Barang:</label>
-            <input name="nama" value="<?php echo $nama_barang; ?>" type="text" class="form-control" placeholder="Masukan nama" required>
+            <input name="nama" value="<?=$row['nama_brng'] ?>" type="text" class="form-control" placeholder="Masukan nama" required>
         </div>
 
         <div class="row">
             <div class="col-sm-12">
                 <div class="form-group">
                     <label>Gambar Barang:</label>
-                    <input name="gambar"  type="file" class="form-control" value="<?php echo $gambar_brng; ?>" required>
+                    <input name="gambar"  type="file" class="form-control" value="<?=$row['gambar_brng'] ?>" required>
                 </div>
             </div>
         </div>
@@ -37,7 +25,7 @@
             <div class="col-sm-12">
                 <div class="form-group">
                     <label>Harga Barang:</label>
-                    <input name="harga" value="<?php echo $harga_barang; ?>" type="text" class="form-control" placeholder="Masukan email" required>
+                    <input name="harga" value="<?=$row['harga_brng'] ?>" type="text" class="form-control" placeholder="Masukan email" required>
                 </div>
             </div>
         </div>
@@ -46,11 +34,11 @@
             <div class="col-sm-12">
                 <div class="form-group">
                     <label>Jenis Barang:</label>
-                    <select class="form-control" name="jenis" value="<?php echo $jenis_barang; ?>" required>
+                    <select class="form-control" name="jenis" value="<?=$row['jenis_brng'] ?>" required>
                     <?php
                         $sql = mysqli_query($con, "SELECT * FROM tb_kategori");
-                        while ($row = mysqli_fetch_array($sql)) {
-                            echo "<option value='$row[jenis_brng]'>$row[jenis_brng]</option>";
+                        while ($row2 = mysqli_fetch_array($sql)) {
+                            echo "<option value='$row2[jenis_brng]'>$row2[jenis_brng]</option>";
                         }
                     ?>
                     </select>
@@ -61,15 +49,15 @@
             <div class="col-sm-12">
                 <div class="form-group">
                     <label>Deskripsi Barang:</label>
-                    <input name="harga" value="<?php echo $desk_barang; ?>" type="text" class="form-control" placeholder="Masukan email" required>
+                    <input name="desk" type="text" class="form-control" placeholder="Masukan email"  value="<?=$row['deskripsi'] ?>" required>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-12">
                 <div class="form-group">
-                    <label>Deskripsi Barang:</label>
-                    <input name="harga" value="<?php echo $jumlah_barang; ?>" type="text" class="form-control" placeholder="Masukan email" required>
+                    <label>Jumlah Barang:</label>
+                    <input name="jumlah" value="<?=$row['jumlah_brng'] ?>" type="text" class="form-control" placeholder="Masukan email" required>
                 </div>
             </div>
         </div>
@@ -78,33 +66,27 @@
     </form>
 
 <?php
+    include '../lib/koneksi.php';
     if (isset($_POST['simpan_edit'])) {
-        //Include file koneksi, untuk koneksikan ke database
-        include '../../lib/koneksi.php';
-        
-        //Fungsi untuk mencegah inputan karakter yang tidak sesuai
-        function input($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
+        $nama = $_POST['nama'];
+        $name = $_FILES['gambar']['name'];
+        $file = $_FILES['gambar']['tmp_name'];
+        move_uploaded_file($file,"../image/$nama");
+        $harga = $_POST['harga'];
+        $jenis = $_POST['jenis'];
+        $desk = $_POST['desk'];
+        $jumlah = $_POST['jumlah']; 
+        if (empty($_FILES['gambar']['name'])) {
+            $sql = mysqli_query($con, "UPDATE tb_barang SET nama_brng = '$nama', gambar_brng = '$name', harga_brng = '$harga', jenis_brng = '$jenis', deskripsi = '$desk' , jumlah_brng = '$jumlah'");
         }
+        // $sql = mysqli_query($con,"UPDATE tb_barang SET nama_brng='$nama',gambar_brng='$file',harga_brng='$harga',jenis_brng='$jenis',deskripsi='$desk',jumlah_brng='$jumlah' WHERE '");
+        // if ($sql) {
+        //     header("Location:../index.php?halaman=ulasan&edit=berhasil");
+        // }
+        // else {
+        //     header("Location:../index.php?halaman=ulasan&edit=gagal");;
 
-        //Query input menginput data kedalam tabel anggota
-        
-        //Mengeksekusi/menjalankan query 
-        $hasil=mysqli_query($con, "UPDATE tb_barang SET 
-                                    ");
-
-
-        //Kondisi apakah berhasil atau tidak dalam mengeksekusi query diatas
-        if ($hasil) {
-            header("Location:../index.php?halaman=ulasan&edit=berhasil");
-        }
-        else {
-            header("Location:../index.php?halaman=ulasan&edit=gagal");;
-
-        }
+        // }
         
     }
-    ?>
+?>
